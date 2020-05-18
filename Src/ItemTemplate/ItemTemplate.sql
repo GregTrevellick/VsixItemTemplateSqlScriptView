f73439220ba69d443928e012854af17ca772eeaa
Greg Trevellick <gtrevellick@googlemail.com>
2020-05-18 20:10:23 +0100

    .

--- Src/ItemTemplate/ItemTemplate.sql
+++ Src/ItemTemplate/ItemTemplate.sql
@@ -1,4 +1,6 @@
-﻿IF NOT EXISTS
+﻿/* Changes to this template can be proposed at https://github.com/GregTrevellick/VsixItemTemplateSqlScriptSchema/blob/master/Src/ItemTemplate/ItemTemplate.sql */
+
+IF NOT EXISTS
 			(SELECT 1 
 			FROM sys.views vws
 			INNER JOIN sys.schemas sch ON sch.schema_id = vws.schema_id 
